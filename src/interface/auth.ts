f2166f46d4b5f632d7dccfd6eff92cd919cd7ff8
long <ltlong2311@gmail.com>
2022-03-20 20:44:56 +0700

    add header

---
+++ src/interface/auth.ts
@@ -0,0 +1,11 @@
+export interface ILoginInfo {
+    username?: string;
+    password?: string;
+}
+
+export interface IUserInfo {
+    id: number;
+    name: string;
+    email: string;
+    token: string;
+}
---
+++ src/interface/news.ts
@@ -0,0 +1,7 @@
+export interface INews {
+    name: string;
+    description: string;
+    image: string;
+    slug: string;
+    isDelete?: boolean;
+}
